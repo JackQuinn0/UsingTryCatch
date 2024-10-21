1affb4bf7eef428987c252f7bdf5a8e90978f6a4
JackQuinn0 <G00395858@atu.ie>
2024-10-21 09:55:46 +0100

    UsingTryCatch Lab done

--- src/main/java/ExceptionHandling.java
+++ src/main/java/ExceptionHandling.java
@@ -1,26 +1,51 @@
 import java.util.Scanner;
 public class ExceptionHandling {
     public static void main(String[] args) {
-        int topNum = 20;
-        int baseNum = 0;
-        int total = 0;
-        Scanner scan = new Scanner(System.in);
 
-    try {
+        System.out.println("Please enter a Number between 0 - 9: ");
+
+        try(Scanner myscan = new Scanner(System.in)){
+            int myNum = -1;
+            while (myNum < 0 || myNum > 9) {
+                String input = myscan.nextLine();
+                try {
+                    myNum = Integer.parseInt(input);
+                    if (myNum >= 0 && myNum <= 9) {
+                        System.out.println("The Number entered is: " + myNum);
+                    } else {
+                        System.out.println("Invalid Number! Please Retry");
+                    }
+                }catch(NumberFormatException e) {
+                        System.out.println("Invalid Number! Please Retry" +e.getMessage());
+                    }
+                }
+            }
+        }
+
+
+    }
+
+
+
+
+
+   /* try {
         System.out.println("Please enter a Number: ");
-        topNum = scan.nextInt();
+        topNum = myscan.nextInt();
         System.out.println("Please enter a second number: ");
-        baseNum= scan.nextInt();
+        baseNum= myscan.nextInt();
         total = topNum / baseNum;
         }
+
         catch(ArithmeticException ae){
             System.out.println("Please enter values greater than 0" +ae.getMessage());
         }
+
     finally{
-        scan.close();
+        myscan.close();
     }
 
         System.out.println("The total is " + total);
 
     }
-}
+}*/
